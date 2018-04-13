d71dd73a8e2f4ab1c5dd874251fcd5908eecd5c5
Nicolas Cohen <nickonet@gmail.com>
2018-04-13 14:48:07 -0700

    Fix font path from webpack

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,8 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import App from './app'
 
+__webpack_public_path__ = "http://localhost:8080/"
+
+
 const root = document.getElementById('app');
 ReactDOM.render(<App pathname={location.pathname}/>, root);
