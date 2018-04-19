754a586807f84515ee0a0bc457ac3b1f047ac261
Nicolas Cohen <nickonet@gmail.com>
2018-04-19 11:29:54 -0700

    Clean round view

--- src/game/RoundView.jsx
+++ src/game/RoundView.jsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Statistic, Table, Grid, Header, Container, Button, Confirm } from 'semantic-ui-react'
 import { withRouter } from 'react-router-dom'
 import { CONTRACTS, SUITS } from '../utils/constants'
-import { pointsNeeded } from '../utils/rules'
+import { pointsNeeded, poigneeDescription } from '../utils/rules'
 
 class RoundView extends React.Component {
 
@@ -11,13 +11,28 @@ class RoundView extends React.Component {
         this.state = { open: false }
     }
 
+    player(round, players, player, index, intermediateTotals) {
+        // Add 1-based rank
+        let rank = 1;
+        players.forEach(p => {
+            if (p !== player && intermediateTotals[p] > intermediateTotals[player]) {
+                rank++;
+            }
+        });
+
+        return <div key={index}>
+            {player} marque {round.scores[player] || 0} points et est {rankInFrench(rank)}.
+        </div>
+    }
+
     render() {
-        if (!round) {
-            return <div id="app-loading"><img src="/images/loader.apng" /></div>
-        }
         const { game, players } = this.props;
         const roundIndex = parseInt(this.props.match.params.rId, 10);
         const round = game.rounds[roundIndex];
+
+        if (!round) {
+            return <div id="app-loading"><img src="/images/loader.apng" /></div>
+        }
         const contract = CONTRACTS.find(i => i.val === round.contract).lab;
         var oudlers = 0;
         if (round.twentyOne) oudlers++;
@@ -25,8 +40,20 @@ class RoundView extends React.Component {
         if (round.excuse) oudlers++;
         const ptsNeeded = pointsNeeded(oudlers)
 
+        // Compute ranks at that point
+        const intermediateTotals = players.reduce((h, p) => {
+            const total = game.rounds.reduce((total, round, idx) => {
+                if (idx <= roundIndex) {
+                    return total + (round.scores[p] || 0);
+                }
+                return total;
+            }, 0);
+            h[p] = total;
+            return h;
+        }, {});
+
         return (
-            <div>
+            <Container>
                 <Header>Partie {roundIndex + 1}</Header>
                 <Confirm content="Voulez-vous vraiment effacer la partie?"
                     onCancel={() => this.setState({ open: false })}
@@ -34,50 +61,55 @@ class RoundView extends React.Component {
                     onConfirm={() => this.props.onRoundDelete(roundIndex)}/>
                 <div className="push-32">
                     {round.bidder} a pris une {contract}
-                {
-                    round.sidekick && 
-                    <span> et a appele {round.sidekick} ({SUITS[round.suitCalled].code})</span>
+                    {
+                        round.sidekick && 
+                        <span> et a appele {round.sidekick} ({SUITS[round.suitCalled].code})</span>
                     }. L'attaque totalise {round.points} points pour {ptsNeeded}.
+                    {
+                        round.poignee && <span> {poigneeDescription(round.poignee)} annoncee!</span>
+                    }
+                    {
+                        round.petitAuBout && <span> Le petit a ete mis au bout.</span>
+                    }
                 </div>
-                <div className="push-32">
+                <div className="push-32 push-up-32">
                     <strong>
                     {
                         (round.points >= ptsNeeded) ?
-                            <strong>La partie est faite de {round.points - ptsNeeded}</strong> :
-                            <strong>La partie est chutee de {ptsNeeded - round.points}</strong>
-                    }
+                            <span>La partie est faite de {round.points - ptsNeeded}</span> :
+                            <span>La partie est chutee de {ptsNeeded - round.points}</span>
+                    }.
                     </strong>
+                    {
+                        round.grandChelem &&
+                            (round.points === 91 ? <span>Grand chelem annonce et fait!</span> :
+                                <span>Grand chelem annonce mais rate.</span>)
+                    }
                 </div>
-                <Table definition>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell />
-                            {
-                                players.map(p => (
-                                    <Table.HeaderCell key={p}>{p}</Table.HeaderCell>
-                                ))
-                            }
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        <Table.Row>
-                            {
-                                players.map((p, j) => (<Table.Cell key={j}>{round.scores[p]}</Table.Cell>))
-                            }
-                        </Table.Row>
-                    </Table.Body>
-                </Table>
-                <Grid className="push-32" columns={2} textAlign="center">
-                    <Grid.Column>
-                        <Button basic onClick={this.props.history.goBack}>Revenir</Button>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Button color="red" onClick={() => this.setState({ open: true })}>Effacer la Partie</Button>
-                    </Grid.Column>
-                </Grid>
-            </div>
+                {
+                    players.map((p, idx) => this.player(round, players, p, idx, intermediateTotals))
+                }
+                <div className="push-32">
+                    <Grid columns={2} textAlign="center">
+                        <Grid.Column>
+                            <Button basic onClick={this.props.history.goBack}>Revenir</Button>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Button color="red" onClick={() => this.setState({ open: true })}>Effacer la Partie</Button>
+                        </Grid.Column>
+                    </Grid>
+                </div>
+            </Container>
         )
     }
 }
+
+function rankInFrench(rank) {
+    if (rank === 1) {
+        return '1er';
+    }
+    return `${rank}eme`;
+}
+
 
 export default withRouter(RoundView)
